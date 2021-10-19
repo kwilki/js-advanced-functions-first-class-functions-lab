93dabe22ee4df91ab40ba0101121ba9cb0d4d696
Kyle <kylewilkinson14@gmail.com>
2021-10-19 20:31:01 +1100

    started

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const returnFirstTwoDrivers = function(){
+    const newArray = [drivers[0], drivers[1]];
+    return newArray;
+}
+
+const returnLastTwoDrivers = function(){
+    const newArray = [drivers[2], drivers[3]];
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(){
+    
+}
