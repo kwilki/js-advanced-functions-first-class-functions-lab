a801be4881e8e8afde90984179419d12c8d5c809
Kyle <kylewilkinson14@gmail.com>
2021-10-20 10:26:02 +1100

    finished a coiple more

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const returnLastTwoDrivers = function(){
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
-const createFareMultiplier = function(){
-    
+const createFareMultiplier = function(multiplier){
+     return function fareMultiplier(value){
+        return multiplier * value
+     }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers, returnFirstTwoDrivers){
+
 }
